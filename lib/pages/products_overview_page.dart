4b891ba4f0a6f591e1b617e998031ed504d89e12
Joao Luiz <joaoluizmarcelinoribeiro@gmail.com>
2023-07-21 14:57:04 -0300

    Componente Grid de Produtos

---
+++ lib/pages/products_overview_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:shop/data/dummy_data.dart';
+
+import '../models/product.dart';
+
+class ProductsOverviewPage extends StatelessWidget {
+  final List<Product> loadedProducts = dummyProducts;
+  ProductsOverviewPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Minha Loja'),
+      ),
+      body: GridView.builder(
+        itemCount: loadedProducts.length,
+        itemBuilder: (ctx, i) => Text(loadedProducts[i].title),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3/2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+        ),
+      ),
+    );
+  }
+}
