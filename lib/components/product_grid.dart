e77d774958acdac861b2afbbcb9d1489d3656d6e
Joao Luiz <joaoluizmarcelinoribeiro@gmail.com>
2023-07-25 18:02:35 -0300

    consumer e provider: adicionando para que foquem unicamente em settar produto como favorito

--- lib/components/product_grid.dart
+++ lib/components/product_grid.dart
@@ -5,6 +5,8 @@ import '../models/product.dart';
 import '../models/product_list.dart';
 
 class ProductGrid extends StatelessWidget {
+  const ProductGrid({super.key});
+
   @override
   Widget build(BuildContext context) {
     final provider = Provider.of<Productlist>(context);
@@ -13,10 +15,10 @@ class ProductGrid extends StatelessWidget {
     return GridView.builder(
       padding: const EdgeInsets.all(10),
       itemCount: loadedProducts.length,
-      itemBuilder: (ctx, i) => ChangeNotifierProvider(
-        create: (_) => loadedProducts[i],
-        child: ProductItem(),
-        ),
+      itemBuilder: (ctx, i) => ChangeNotifierProvider.value(
+        value: loadedProducts[i],
+        child: const ProductItem(),
+      ),
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 2,
         childAspectRatio: 3 / 2,
