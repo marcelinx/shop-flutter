7ffd7c4f31e52ecd13defa9d5153fb0400fd59e5
Joao Luiz <joaoluizmarcelinoribeiro@gmail.com>
2023-07-28 16:54:44 -0300

    adicionando filtro de favoritos 01

--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,17 +1,47 @@
 import 'package:flutter/material.dart';
-
+import 'package:provider/provider.dart';
+import 'package:shop/models/product_list.dart';
 import '../components/product_grid.dart';
+
+enum FilterOptions {
+  Favorite,
+  All,
+}
 
 class ProductsOverviewPage extends StatelessWidget {
   const ProductsOverviewPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<Productlist>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Minha Loja'),
+        actions: [
+          PopupMenuButton(
+            icon: const Icon(Icons.more_horiz),
+            itemBuilder: (_) => [
+              const PopupMenuItem(
+                value: FilterOptions.Favorite,
+                child: Text('Somente Favoritos'),
+              ),
+              const PopupMenuItem(
+                value: FilterOptions.All,
+                child: Text('Todos'),
+              ),
+            ],
+            onSelected: (FilterOptions selectedValue) {
+              if(selectedValue == FilterOptions.Favorite) {
+                provider.showFavoriteOnly();
+              }else {
+                provider.showAll();
+              }
+            },
+          )
+        ],
       ),
-      body: ProductGrid(),
+      body: const ProductGrid(),
     );
   }
 }
