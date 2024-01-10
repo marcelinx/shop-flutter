fc061ccab2c3f9889913db310a11478b21329dfa
Joao Luiz <joaoluizmarcelinoribeiro@gmail.com>
2024-01-10 12:31:02 -0300

    Entendendo o envio de requisicoes POST no firebase

--- lib/data/dummy_data.dart
+++ lib/data/dummy_data.dart
@@ -7,7 +7,7 @@ final dummyProducts = [
     description: 'A red shirt - it is pretty red!',
     price: 29.99,
     imageUrl:
-        'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
+        'https://i.ebayimg.com/images/g/qbUAAOSwmjVZ0-8V/s-l1600.jpg',
   ),
   Product(
     id: 'p2',
--- lib/models/product_list.dart
+++ lib/models/product_list.dart
@@ -1,10 +1,13 @@
+import 'dart:convert';
 import 'dart:math';
 
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:shop/data/dummy_data.dart';
 import 'package:shop/models/product.dart';
 
 class ProductList with ChangeNotifier {
+  final _baseUrl = 'https://shop-marcelinx-default-rtdb.firebaseio.com';
   final List<Product> _items = dummyProducts;
 
   List<Product> get items => [..._items];
@@ -34,6 +37,17 @@ class ProductList with ChangeNotifier {
   }
 
   void addProduct(Product product) {
+    http.post(
+      Uri.parse('$_baseUrl/products.json'),
+      body:jsonEncode({
+        "name": product.name,
+        "description": product.description,
+        "price": product.price,
+        "imageUrl": product.imageUrl,
+        "isFavorite": product.isFavorite,
+      })
+    );
+
     _items.add(product);
     notifyListeners();
   }
