b0cff479948a4a006d3c9ec0f52989e3fe26be5b
Joao Luiz <joaoluizmarcelinoribeiro@gmail.com>
2024-01-12 16:09:43 -0300

    armazenando os pedidos no firebase

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,4 +1,6 @@
 class Constants {
   static const productBaseUrl =
       'https://shop-marcelinx-default-rtdb.firebaseio.com/products';
+  static const orderBaseUrl =
+      'https://shop-marcelinx-default-rtdb.firebaseio.com/order';
 }
