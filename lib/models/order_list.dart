386c60736f21f7e2ac052332f9dcba6fb97dee12
Joao Luiz <joaoluizmarcelinoribeiro@gmail.com>
2024-01-12 16:30:13 -0300

    carregando os pedidos

--- lib/models/order_list.dart
+++ lib/models/order_list.dart
@@ -1,12 +1,10 @@
 import 'dart:convert';
-import 'dart:math';
-
 import 'package:flutter/material.dart';
-import 'package:shop/models/order.dart';
 import 'package:http/http.dart' as http;
+import 'package:shop/models/cart.dart';
+import 'package:shop/models/cart_item.dart';
+import 'package:shop/models/order.dart';
 import 'package:shop/utils/constants.dart';
-
-import 'cart.dart';
 
 class OrderList with ChangeNotifier {
   final List<Order> _items = [];
@@ -19,36 +17,67 @@ class OrderList with ChangeNotifier {
     return _items.length;
   }
 
+  Future<void> loadOrders() async {
+    _items.clear();
+
+    final response = await http.get(
+      Uri.parse('${Constants.orderBaseUrl}.json'),
+    );
+    if (response.body == 'null') return;
+    Map<String, dynamic> data = jsonDecode(response.body);
+    data.forEach((orderId, orderData) {
+      _items.add(
+        Order(
+          id: orderId,
+          date: DateTime.parse(orderData['date']),
+          total: orderData['total'],
+          products: (orderData['products'] as List<dynamic>).map((item) {
+            return CartItem(
+              id: item['id'],
+              productId: item['productId'],
+              name: item['name'],
+              quantity: item['quantity'],
+              price: item['price'],
+            );
+          }).toList(),
+        ),
+      );
+    });
+    notifyListeners();
+  }
+
   Future<void> addOrder(Cart cart) async {
     final date = DateTime.now();
-    final _url = Constants.orderBaseUrl;
 
     final response = await http.post(
-      Uri.parse('$_url.json'),
+      Uri.parse('${Constants.orderBaseUrl}.json'),
       body: jsonEncode(
         {
           'total': cart.totalAmount,
           'date': date.toIso8601String(),
           'products': cart.items.values
-              .map((cartItem) => {
-                    'id': cartItem.id,
-                    'productId': cartItem.productId,
-                    'name': cartItem.name,
-                    'quantity': cartItem.quantity,
-                    'price': cartItem.price,
-                  })
-              .toList()
+              .map(
+                (cartItem) => {
+                  'id': cartItem.id,
+                  'productId': cartItem.productId,
+                  'name': cartItem.name,
+                  'quantity': cartItem.quantity,
+                  'price': cartItem.price,
+                },
+              )
+              .toList(),
         },
       ),
     );
+
     final id = jsonDecode(response.body)['name'];
     _items.insert(
       0,
       Order(
         id: id,
         total: cart.totalAmount,
-        products: cart.items.values.toList(),
         date: date,
+        products: cart.items.values.toList(),
       ),
     );
 
