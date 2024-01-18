6039e9cc878b1da0b59da348a5d9d80b88c8d7af
Joao Luiz <joaoluizmarcelinoribeiro@gmail.com>
2024-01-18 14:45:09 -0300

    associando o favorito ao usuario

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,6 +1,8 @@
 class Constants {
   static const productBaseUrl =
       'https://shop-marcelinx-default-rtdb.firebaseio.com/products';
+  static const userFavoriteUrl =
+      'https://shop-marcelinx-default-rtdb.firebaseio.com/userFavorite';
   static const orderBaseUrl =
       'https://shop-marcelinx-default-rtdb.firebaseio.com/order';
   static const webApiKey = 'AIzaSyCIZGVx9erDztMML1XDIDxoXBLjdWiVRGU';
